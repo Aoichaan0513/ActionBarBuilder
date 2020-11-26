fd62beff362e562a078459a9abfb2c584156bf63
Aoichaan0513 <aoichaan0513@gmail.com>
2020-11-27 02:05:53 +0900

    [Upgrade] ライブラリのバージョンを更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 val groupId = "jp.aoichaan0513"
 group = groupId
-version = "1.1.0"
+version = "1.1.1"
 
 repositories {
     mavenCentral()
@@ -16,7 +16,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("jp.aoichaan0513", "Kotlin_Utils", "1.1.0")
+    implementation("jp.aoichaan0513", "Kotlin_Utils", "1.1.2")
 }
 
 tasks.withType<KotlinCompile> {
